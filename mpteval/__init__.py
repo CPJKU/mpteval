ffa6248cd1444d6acc816e08c0b4b745d9eac95f
huispaty <hu.patriciaziyi@hotmail.com>
2026-03-19 14:52:45 +0100

    enable imports

--- mpteval/__init__.py
+++ mpteval/__init__.py
@@ -4,28 +4,30 @@
 import pkg_resources
 
 # preprocessing and utils
-from . import config
-from . import preprocess
+from mpteval.core import config
+from mpteval import utils
+from mpteval.utils import (
+    is_monophonic,
+    midi_to_piano_note,
+    plot_piano_roll,
+    create_note_list,
+    alignment_list_to_df,
+    group_by_structure,
+    pairwise_distance_matrix,
+    accumulated_cost_matrix,
+    optimal_warping_path,
+    dynamic_time_warping,
+    fast_dynamic_time_warping,
+    greedy_note_alignment,
+    notewise_alignment,
+)
 
-# alignment
-from . import dtw_align
-from . import dtw_dist
+from mpteval.preprocessing import preprocess
+from mpteval.alignment import dtw_align, dtw_dist
+from mpteval.clustering import cluster, cluster_eval
 
-# clustering
-from . import cluster
-from . import cluster_eval
-
-# information retrieval (objective) and perceptually motivated (subjective) metrics
-from . import objective
-from . import subjective
-
-# performance metrics
-from . import articulation
-from . import dynamics
-from . import harmony
-from . import timing
-
-from . import utils
+from mpteval.features import articulation, dynamics, harmony, timing
+from mpteval.metrics import objective, subjective
 
 __version__ = pkg_resources.get_distribution("mpteval").version
 
