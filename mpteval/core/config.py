f561b9bc41fab17bff4d267838d4bb0e66a06d50
huispaty <hu.patriciaziyi@hotmail.com>
2026-05-21 09:12:34 +0200

    default configs used for ismir24

--- mpteval/core/config.py
+++ mpteval/core/config.py
@@ -1,9 +1,9 @@
 PERF_PIANO_ROLL_PARAMS = {
     "time_unit": "sec",
-    "time_div": 100, # frames per time_unit, i.e., if time_unit='sec' and time_div=100, then fps=100 frames/sec (each frame = 10ms)
+    "time_div": 100,       # frames per time_unit; if time_unit=sec and time_div=100, then fps=100 (each frame = 10ms)
     "onset_only": False,
-    "piano_range": True,  # 88 x num_time_steps
-    "time_margin": 0,  # amount of padding before and after piano roll
+    "piano_range": True,   # 88 x num_time_steps
+    "time_margin": 0,      # amount of padding before and after piano roll
     "return_idxs": False,
 }
 
